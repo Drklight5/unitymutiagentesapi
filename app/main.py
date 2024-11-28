0858f41a5e3dcb1a1b34f8c07bc4b023e24e6869
Valeria <valeria050603@hotmail.com>
2024-11-27 22:21:29 -0600

    a

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ import json
  
 app = Flask(__name__)
  
-# @app.route("/")
-# def home_view():
-#         return "<h1>Welcome to Geeks for Geeks</h1>"
+@app.route("/")
+def home_view():
+        return "<h1>Welcome to Geeks for Geeks</h1>"
 
 
 
